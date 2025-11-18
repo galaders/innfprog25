04fa16a7c46cd64877c5b887ebe08aab29a48a24
galaders <kjetil.engvold@outlook.com>
2025-11-18 12:44:53 +0100

    asdasd

---
+++ eksamen_innfprog2025/oppgave1/actions.js
@@ -0,0 +1,16 @@
+const kursoversikt = ["Programmering 1, Webutvikling, 3D-modellering, 3D-Animasjon, 3D-Spillutvikling, Kunstig intelligens, Maskinlæring,Databaser, Operativsystemer, Webdesign, Responsiv Utvikling, Grunnleggende JavaScript, Fontend_utvikling, CSS og design, Python og web, Systemutvikling, Datakommunikasjon, Datasikkerhet,"]
+console.log(kursoversikt)
+const navn = [""] //21 1 rekotr 2 dekaner  3 vaktmestere 5 profseorer 10 lektor
+const stillinger =[""] //5
+const kontor = ["c308 til c329"] // 21
+const epost = [""] // 21
+const dobbelStillinger = [""] // 3
+const undervisere = [""] // 15
+console.log(kontor)
+const employees = [
+            {name: "Hans Hansen", position: "Programmerer", email: "hans.hansen@email.com", office: "316", image: "man"},
+            {name: "Jens Jensen", position: "Programmerer", email: "jens.jensen@email.com", office: "323", image: "man2"},
+            {name: "Trine Trinesdotter", position: "Programmerer", email: "trine.trinesdotter@email.com", office: "304", image: "woman"},
+        ]
+console.log(employees)
+document.getElementById("employees").innerHTML = HTML_employees
